e84c2ea0c83d676552db820c13d62888afc80c1d
Jisuel <jesuel1@outlook.com>
2019-11-08 15:40:54 -0400

    Añadiendo el proyecto al repositorio

---
+++ Mustachio/Controllers/NosotrosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mustachio.Controllers
+{
+    public class NosotrosController : Controller
+    {
+        // GET: Nosotros
+        public ActionResult Nosotros()
+        {
+            return View();
+        }
+    }
+}
